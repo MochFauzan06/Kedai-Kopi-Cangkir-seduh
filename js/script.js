42d5d068f8ccd185cf482a1745e877592b89cbc4
Fauzan <mochfauzan062002@gmail.com>
2023-11-15 10:45:49 +0700

    website kedai kopi cangkir seduh

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// toggle (yang tadi ada jadi gak ada, yang tadi gak ada yadi ada) class active
+const navbarnav = document.querySelector(".navbar-nav");
+
+// ketika hamburger menu diklik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarnav.classList.toggle("active");
+};
+
+
+// klik diluar sidebar menghilangkan nav nya
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarnav.contains(e.target)) {
+    navbarnav.classList.remove("active");
+  }
+});
